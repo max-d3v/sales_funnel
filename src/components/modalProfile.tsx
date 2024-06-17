7b1e8fede3d43b68666c93121fcff6d66be2eb02
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-06-17 15:39:09 -0300

    minor fixes / tst ajax

--- src/components/modalProfile.tsx
+++ src/components/modalProfile.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { CiLogout } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
 import { ajax } from "../ajax/ajax";
-//import { IoEnter } from "react-icons/io5";
+import { IoEnter } from "react-icons/io5";
 
 export function ModalProfile() {
     const { user, signed } = useContext(AuthContext);
@@ -43,7 +43,7 @@ export function ModalProfile() {
     return (
         <div className="flex-col gap-4 h-auto w-auto p-6 bg-white rounded-md shadow-2xl absolute flex items-center justify-center outline-0.5 right-3 z-50"> 
           <div className="flex items-center gap-2"> <CiUser />  <p className="m-0 font-semibold text-md">{signed ? name : "" }</p></div>
-         {/* <div className=" customBorder shadow-sm flex items-center justify-center gap-2 hover:text-black text-white bg-black cursor-pointer hover:bg-white p-2 rounded-md w-full hover:shadow-lg transiton-all duration-300" onClick={() => navigate("/gestoria")} > <IoEnter /> <p className="font-semibold m-0 " >Gestoria</p></div>*/}
+          <div className=" customBorder shadow-sm flex items-center justify-center gap-2 hover:text-black text-white bg-black cursor-pointer hover:bg-white p-2 rounded-md w-full hover:shadow-lg transiton-all duration-300" onClick={() => navigate("/gestoria")} > <IoEnter /> <p className="font-semibold m-0 " >Gestoria</p></div>
           <div className="flex items-center justify-center gap-2 cursor-pointer p-2 rounded-md bg-red-500 w-full hover:bg-red-800 hover:shadow-lg transiton-all duration-300" onClick={() => handleLogOut()}><CiLogout /><p className="text-white font-semibold m-0" >Sair</p></div>
         </div>
     )
