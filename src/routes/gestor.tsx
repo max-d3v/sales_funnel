52e8495a0a2977dc14dbbe4875a0061814688417
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-06-19 14:11:19 -0300

    direct to home if user tries to access gestoria without gestor perms

--- src/routes/gestor.tsx
+++ src/routes/gestor.tsx
@@ -1,17 +1,18 @@
 import { ReactNode } from "react";
 import SearchContext from "../components/layout";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 interface GestorProps {
     children: ReactNode;
 }
 
 export function Gestor({children}: GestorProps): any {
     const {isGestor} = useContext(SearchContext);
-
+    const navigate = useNavigate();
     if (isGestor) {
         return children;
     }
     else {
-        return "";
+        return navigate("/");
     }
 }
