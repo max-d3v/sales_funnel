a7b7c8f9eb97d7316b1d12165b4de415af17bdc9
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-07-12 08:55:29 -0300

    tests

--- portainer-deploy-script.js
+++ portainer-deploy-script.js
@@ -92,7 +92,7 @@ new class DeployPortainer {
             // Filtrar pelo nome da imagem
 
             containers.forEach(container => {
-                console.log("Comparou a imagem: " + container.Image + " com a imagem: copapel/" + this.Imagem);
+                console.log("Comparouu a imagem: " + container.Image + " com a imagem: copapel/" + this.Imagem);
                 console.log(container.Imagem == "copapel/" + this.Imagem );
             });
             
